6cd6b3e75e06f67758c9f2d94ecce20342c97e94
adamelyoumi <adam.elyoumi@hotmail.fr>
2023-10-27 14:04:59 -0400

    path modification

--- install.ps1
+++ install.ps1
@@ -2,7 +2,9 @@
 $venvName="bot2"
 
 Write-Host "Creation de l'environnement virtuel $venvName..."
-& c:\Users\Biran\AppData\Local\Programs\Python\Python311\python.exe -m venv $venvName
+$currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.Split("\")[1]
+$pythonPath = "c:\Users\$currentUser\AppData\Local\Programs\Python\Python311\python.exe"
+& $pythonPath -m venv $venvName
 
 Write-Host "Activation de l'environnement virtuel $venvName..."
 & "$venvName\Scripts\Activate"
